d0101b769529629f4d2ac0d1bdee7f6ad4cd3635
coding-with-AHRK <69730619+coding-with-AHRK@users.noreply.github.com>
2020-08-16 02:20:13 -0700

    Add files via upload

---
+++ commit-tut-file.py
@@ -0,0 +1,11 @@
+print('hello hassan')
+#this is single line comment
+print('double line comment or mulitiple line comments are required """ triple qoute from start to end')
+"""
+
+this is multiline code syntax 
+
+
+"""
+
+
